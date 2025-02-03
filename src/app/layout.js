5f2683781544f3bfab77d862445c06a85a5fa27b
AakashGaurabhyperface <aakash.gaurab@hyperface.co>
2025-02-03 16:54:47 +0530

    Added google analytics.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+
+        <GoogleAnalytics gaId="G-1RZS6RXSMX" />
 
         <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
         <script src="https://files.bpcontent.cloud/2025/02/03/09/20250203090804-02IK6G2O.js"></script>
