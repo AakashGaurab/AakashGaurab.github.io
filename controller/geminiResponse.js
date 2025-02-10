0e0eb62ab5216f74b0f510f2234faef93a961347
AakashGaurabhyperface <aakash.gaurab@hyperface.co>
2025-02-10 18:48:30 +0530

    Fixed mistake

--- controller/geminiResponse.js
+++ controller/geminiResponse.js
@@ -141,7 +141,7 @@ const functions = {
   },
 };
 
-const genAI = new GoogleGenerativeAI(`${process.env.GEMINI_API_KEY}`);
+const genAI = new GoogleGenerativeAI(`${process.env.MY_SECRET_API_KEY}`);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
